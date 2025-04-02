348d5f7b438ea0a5ff386409bcab95045fc49b40
vishalmet <vishalmet003@gmail.com>
2025-04-02 20:46:59 +0530

    feat: added a custom cursor and few other style comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Launch from "./pages/Launch.jsx";
+import Cursor from "../src/components/cursor.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // <Cursor>
+    <Router>
+      <Routes>
+        <Route 
+          path="/"
+          element={
+            <Launch
+              count={75}
+              gravity={0.7}
+              friction={0.8}
+              wallBounce={0.95}
+              followCursor={false}
+            />
+          }
+        />
+        {/* <Route path="/works" element={<h1>Works Page</h1>} />
+        <Route path="/contact" element={<h1>Contact Page</h1>} /> */}
+      </Routes>
+    </Router>
+    // </Cursor>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Cursor from "../src/components/cursor.jsx"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+  <Cursor />
     <App />
   </StrictMode>,
 )
