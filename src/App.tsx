8adc33995b81f79d5a4a7a959b80e9865bcd6316
vishalmet <vishalmet003@gmail.com>
2025-04-03 00:00:27 +0530

    refactored the routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,23 @@ import { Navbar } from "./components/Navbar.tsx";
 function App() {
   return (
     <div className="outfit-font">
-    <Router>
+    {/* <Router> */}
     <Navbar />
-      <Routes>
+      {/* <Routes>
         <Route 
           path="/"
-          element={
-            <HeroGeometric badge="Vishal Aakash"
+          element={ */}
+          
+            <HeroGeometric 
+            badge="Vishal Aakash"
             title1 = "Crafting intuitive Web3 frontends,"
             title2 = "powered by robust full-stack solutions." />
-          }
-        />
+          {/* }
+        /> */}
         {/* <Route path="/works" element={<h1>Works Page</h1>} />
         <Route path="/contact" element={<h1>Contact Page</h1>} /> */}
-      </Routes>
-    </Router>
+      {/* </Routes> */}
+    {/* </Router> */}
     </div>
   );
 }
