329beb72e55af123c0998b9c2de10c1a6befdcaf
vishalmet <vishalmet003@gmail.com>
2025-04-02 23:59:54 +0530

    added framer-motion in optimzed deps

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    include: ['framer-motion'],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
