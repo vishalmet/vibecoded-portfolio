5e802e1f51b5892a94673ca1b32ae3c59ed4e027
vishalmet <vishalmet003@gmail.com>
2025-04-08 20:16:09 +0530

    refactored App component to integrate Launch and About pages, removing commented routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import { HeroGeometric } from "./pages/Launch.tsx";
+import { Launch } from "./pages/Launch.tsx";
 import { Navbar } from "./components/Navbar.tsx";
-
+import About from "./pages/About.tsx";
 function App() {
   return (
     <div className="outfit-font">
-    {/* <Router> */}
-    <Navbar />
-      {/* <Routes>
-        <Route 
-          path="/"
-          element={ */}
-          
-            <HeroGeometric 
-            badge="Vishal Aakash"
-            title1 = "Crafting intuitive Web3 frontends,"
-            title2 = "powered by robust full-stack solutions." />
-          {/* }
-        /> */}
-        {/* <Route path="/works" element={<h1>Works Page</h1>} />
-        <Route path="/contact" element={<h1>Contact Page</h1>} /> */}
-      {/* </Routes> */}
-    {/* </Router> */}
+      <Navbar />
+      <div className="" id="home">
+      <Launch
+        badge="Vishal Aakash"
+        title1="Crafting intuitive Web3 frontends,"
+        title2="powered by robust full-stack solutions." />
+        </div>
+      <div className="" id="about">
+        <About />
+      </div>
+
     </div>
   );
 }
