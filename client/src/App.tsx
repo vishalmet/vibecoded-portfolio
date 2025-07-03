8b5f8c40ce352afe66f7fd4174ee3509f165c89f
vishalmet <vishalmet003@gmail.com>
2025-07-03 11:58:34 +0530

    Refactor App component to use React Router: integrate routing for admin login and dashboard pages, enhancing navigation structure and organization.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Contact } from "lucide-react";
 import "./App.css";
 import { Navbar } from "./components/Navbar.tsx";
@@ -6,24 +6,37 @@ import About from "./pages/About.tsx";
 import Launch from "./pages/Launch.tsx";
 import Projects from "./pages/Projects.tsx";
 import Experience from "./pages/Experience.tsx";
+import AdminLogin from "./pages/admin/Login.tsx";
+import AdminDashboard from "./pages/admin/Dashboard.tsx";
 
 function App() {
   return (
-    <div className="outfit-font">
-      <Navbar />
-      <div className="" id="home">
-      <Launch />
-        </div>
-      <div className="  " id="about">
-        <About />
-      </div>
-      <div className="  " id="projects">
-        <Projects />
-      </div>
-      <div className="  " id="experience">
-        <Experience />
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="outfit-font">
+              <Navbar />
+              <div className="" id="home">
+                <Launch />
+              </div>
+              <div className="  " id="about">
+                <About />
+              </div>
+              <div className="  " id="projects">
+                <Projects />
+              </div>
+              <div className="  " id="experience">
+                <Experience />
+              </div>
+            </div>
+          }
+        />
+          <Route path="/admin/login" element={<AdminLogin  />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
